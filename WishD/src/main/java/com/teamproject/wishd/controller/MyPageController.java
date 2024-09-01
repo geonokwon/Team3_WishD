202e9f476792fdd4074eb0e37058dd48346b5eea
user <user@son>
2024-09-01 19:39:21 +0900

    [2024.09.01 손승경] 내정보탭 데이터베이스 작업 진행중, MyPageMapper.xml 파일, MyPageDAO.java 파일 생성

--- WishD/src/main/java/com/teamproject/wishd/controller/MyPageController.java
+++ WishD/src/main/java/com/teamproject/wishd/controller/MyPageController.java
@@ -1,12 +1,15 @@
 package com.teamproject.wishd.controller;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.teamproject.wishd.domain.MemberDTO;
 import com.teamproject.wishd.service.MyPageService;
 
 @Controller
@@ -14,18 +17,26 @@ import com.teamproject.wishd.service.MyPageService;
 public class MyPageController {
 	
 	@Inject
-	MyPageService myPageService;
+	private MyPageService myPageService;
 	
 	@GetMapping("mypage")
-	public String mypage() {
+	public String mypage(HttpSession session, Model model) {
 //		System.out.println("MyPageController - mypage");
+		MemberDTO memberDTO = myPageService.getMember((String)session.getAttribute("user_no"));
+		System.out.println(memberDTO);
+		model.addAttribute("memberDTO", memberDTO);
 		return "/mypage/mypage";
 	}
 	
 	@PostMapping("mypage/mypageUpdatePro")
-	public String mypageUpdatePro() {
+	public String mypageUpdatePro(MemberDTO memberDTO) {
 //		System.out.println("MyPageController - mypageUpdatePro");
-		return "";
+		MemberDTO memberDTO2 = myPageService.userCheck(memberDTO);
+		if(memberDTO2 != null) {
+			myPageService.updateMember(memberDTO);
+			return "redirect:/mypage";
+		}
+		return "redirect:/mypage";
 	}
 	
 	@PostMapping("mypage/myuserDeletePro")
