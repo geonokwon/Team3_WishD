8218d8733bad35d979f9d6804414afbbdcf8ebe0
user <user@son>
2024-09-02 23:52:35 +0900

    [2024.09.02 손승경] 패키지명 변경으로 MyPageMapper.xml 수정

--- WishD/src/main/java/com/teamproject/controller/MyPageController.java
+++ WishD/src/main/java/com/teamproject/controller/MyPageController.java
@@ -1,4 +1,4 @@
-package com.teamproject.wishd.controller;
+package com.teamproject.controller;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpSession;
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.teamproject.wishd.domain.MemberDTO;
-import com.teamproject.wishd.service.MyPageService;
+import com.teamproject.domain.MemberDTO;
+import com.teamproject.service.MyPageService;
 
 @Controller
 @RequestMapping("/")
@@ -23,14 +23,14 @@ public class MyPageController {
 	public String mypage(HttpSession session, Model model) {
 //		System.out.println("MyPageController - mypage");
 		MemberDTO memberDTO = myPageService.getMember((String)session.getAttribute("user_no"));
-		System.out.println(memberDTO);
 		model.addAttribute("memberDTO", memberDTO);
 		return "/mypage/mypage";
 	}
 	
 	@PostMapping("mypage/mypageUpdatePro")
-	public String mypageUpdatePro(MemberDTO memberDTO) {
+	public String mypageUpdatePro(HttpSession session, MemberDTO memberDTO) {
 //		System.out.println("MyPageController - mypageUpdatePro");
+		memberDTO.setUser_no((int)(1)); // "session.getAttribute(user_no)"
 		MemberDTO memberDTO2 = myPageService.userCheck(memberDTO);
 		if(memberDTO2 != null) {
 			myPageService.updateMember(memberDTO);
