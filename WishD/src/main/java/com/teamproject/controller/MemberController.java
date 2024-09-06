fc814bdd3e850fe7b3cbd7c8e6320b4a63f4f0aa
ziini2 <rksvusgo123@naver.com>
2024-09-06 16:04:13 +0900

    [24.09.06 - 은진] 컨트롤러 이름 변경, login.jsp 경로 재설정

---
+++ WishD/src/main/java/com/teamproject/controller/MemberController.java
@@ -0,0 +1,41 @@
+package com.teamproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+@RequestMapping("/")
+public class LoginController {
+	
+	@GetMapping("/login")
+	public String login() {  
+		System.out.println("LoginController login()");
+		
+		// WEB-INF/veiws/폴더/파일이름.jsp
+		return "/member/login";
+	}
+	
+	
+	
+	@GetMapping("/insert")
+	public String insert() {
+		
+		System.out.println("LoginController login()");
+		
+		return "/login/insert";
+		
+	}
+	
+//	@PostMapping("/insertPro")
+//	public String insertPro(LoginDTO loginDTO) {
+//		
+//		System.out.println("LoginController loginPro");
+//		System.out.println(loginDTO);
+//		
+//	}
+
+}
