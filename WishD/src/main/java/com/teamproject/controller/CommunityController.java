1d9d3316925e0450225b5ea4c5497be7a3d11050
wprk7 <wprk7@DESKTOP-QFE6555>
2024-09-11 16:04:28 +0900

    [2024.09.11 제가인] 컨트롤러 수정

--- WishD/src/main/java/com/teamproject/controller/CommunityController.java
+++ WishD/src/main/java/com/teamproject/controller/CommunityController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 	public class CommunityController {
 
-@GetMapping("Community")
+@GetMapping("community")
 	public String community() {
 		
 		return "community/notice";
