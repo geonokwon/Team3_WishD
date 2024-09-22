3b096ec80dd60d2aa058ff3dcced8ec3f259492e
geonokwon <furitstore1@naver.com>
2024-09-22 16:07:00 +0900

    [24.09.22 - 권건오] 회원가입 수정본 이메일인증 완료

--- WishD/src/main/java/com/teamproject/controller/EmailController.java
+++ WishD/src/main/java/com/teamproject/controller/EmailController.java
@@ -1,58 +1,33 @@
 package com.teamproject.controller;
 
+import com.teamproject.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
-import java.util.Random;
+import java.util.Map;
 
 @Controller
 public class EmailController {
-    @Autowired
-    private JavaMailSender mailSender;
 
-    //인증코드 생성 메서드
-    private String generateCode(){
-        Random random = new Random();
-        int code = random.nextInt(899999) + 100000;
-        return String.valueOf(code);
-    }
+    @Autowired
+    private EmailService emailService;
+
+
 
     @PostMapping("/emailSendCode")
     @ResponseBody
-    public String emailSendCode(@RequestParam("userEmail")String email, Model model) {
-        System.out.println(email);
-        String code = generateCode();
-        model.addAttribute("emailCode", code);
+    public Object emailSendCode(@RequestParam("userEmail")String email) {
+        System.out.println("EmailController -> emailSendCode()");
 
-        try {
-            System.out.println("이메일전송 try 문");
-            MimeMessage message = mailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
-
-            //보내는 이메일을 설정값에서 불러오기
-            helper.setFrom("furitstore1@gmail.com");
-            helper.setTo(email);
-            helper.setSubject("WishD 인증코드 입니다.");
-            helper.setText("인증코드 입니다 : " + code, true);
-
-            //이메일 전송
-            mailSender.send(message);
-            System.out.println("이메일전송 완료");
-            model.addAttribute("emailCode", code);
-            return "success";
-        } catch (MessagingException e) {
-            e.printStackTrace();
-            return "failed";
+        Map<String, Object> result = emailService.sendMail(email);
+//        System.out.println(result.get("code"));
+        if (result.get("status").equals("success")) {
+            return result.get("code");
         }
+        return result.get("status").toString();
     }
 
 }
